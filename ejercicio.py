8b88547f09357cc33457fe2ef481df66974fe307
Default user <l.wandurraga@uniandes.edu.co>
2019-10-22 18:47:35 +0000

    Codigo

---
+++ ejercicio.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pylab as plt
+a=np.linspace(-2*np.pi,2*np.pi,100)
+b=[]
+for i in range(len(a)):
+    b.append(np.sin(a[i]))
+plt.plot(a,b)
+plt.savefig('seno.png')
